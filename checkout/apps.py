ce79787697059bbaa873691d870923da15de9c13
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-03-06 13:49:08 +0330

    add callback t olist of urls

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,5 +1,16 @@
 import oscar.apps.checkout.apps as apps
+from django.urls import path
 
 class ZarrinCheckoutConfig(apps.CheckoutConfig):
     name = 'django_oscar_zarinpal_gateway.checkout'
+    def ready(self):
+        super().ready()
+        from .views import CheckZarrinPalCallBack
+        self.zarinpal_callback = CheckZarrinPalCallBack
 
+    def get_urls(self):
+        urls = super().get_urls()
+        urls += [
+            path('callback/', self.zarinpal_callback.as_view(), name='extra'),
+        ]
+        return self.post_process_urls(urls)
