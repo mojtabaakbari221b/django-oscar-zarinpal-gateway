e7e579c33ac16e8e2d366dd7635dfe5c6d4e9a79
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-03-08 14:29:26 +0330

    init override detailview

--- checkout/views.py
+++ checkout/views.py
@@ -1,28 +1,17 @@
 from oscar.apps.checkout.views import PaymentDetailsView as CorePaymentDetailsView
-from oscar.apps.payment.exceptions import PaymentError
-from suds.client import Client
-from django_oscar_zarinpal_gateway.settings import (
-    ZARINPAL_WEBSERVICE,
-    MMERCHANT_ID,
-    DESCRIPTION,
-)
 from django.views import View
 from django.http import HttpResponse
+from django.http import HttpResponse
+from .gateway import  do_pay
+import logging
+logger = logging.getLogger('oscar.checkout')
+
 
 class PaymentDetailsView(CorePaymentDetailsView):
     def handle_payment(self, order_number, order_total, **payment_kwargs):
-        client = Client(ZARINPAL_WEBSERVICE)
-        result = client.service.PaymentRequest(MMERCHANT_ID,
-                                            1000,
-                                            DESCRIPTION,
-                                            self.request.user.email,
-                                            None,
-                                            user_data_dictionary.get("call_back_url"))
-        print(order_number)
-        print(order_total)
-        raise PaymentError("hello")
+        return do_pay(self.request , order_number , order_total)
 
 class CheckZarrinPalCallBack(View):
     def get(self, request):
-        # <view logic>
-        return HttpResponse('result')    
+        return HttpResponse(f'result {request.GET["order_number"]}')    
+
