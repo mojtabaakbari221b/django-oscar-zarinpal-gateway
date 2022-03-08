1a7345bddecfef54bd552e149d3f3b1399f27206
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-03-08 15:36:51 +0330

    write first call back response

--- checkout/gateway.py
+++ checkout/gateway.py
@@ -2,6 +2,8 @@ from suds.client import Client
 from oscar.apps.payment.exceptions import (
     GatewayError,
     RedirectRequired,
+    UserCancelled,
+    InsufficientPaymentSources,
 )
 from django.urls import reverse
 from django_oscar_zarinpal_gateway.settings import (
@@ -12,10 +14,11 @@ from django_oscar_zarinpal_gateway.settings import (
 )
 
 def do_pay(request, order_number , order_total):
+    # raise GatewayError
     client = Client(WEBSERVICE)
     # if order_total.currency != 'IRR' : # check currency is iranian RIAL
     #     raise InvalidGatewayRequestError("while you use zarinpal-gateway, you shoud use IRR currency")
-    redirect_url = DOMAIN + reverse('checkout:zarinpal-callback') + f'?order_number={order_number}'
+    redirect_url = DOMAIN + reverse('checkout:zarinpal-callback') + f'?order_number={order_number}&amount={order_total.incl_tax}'
     result = client.service.PaymentRequest(
         MMERCHANT_ID,
         order_total.excl_tax * 30000,
@@ -29,3 +32,23 @@ def do_pay(request, order_number , order_total):
         raise RedirectRequired(url=url)
     else:
         raise GatewayError
+
+def check_call_back(request):
+    if request.GET.get('Status') == 'OK':
+        client = Client(WEBSERVICE)
+
+        amount = request.GET.get('amount')
+        
+        result = client.service.PaymentVerification(
+            MMERCHANT_ID,
+            request.GET.get('Authority'),
+            amount,
+        )
+
+        if result.Status == 100 or result.Status == 101 :
+            return True
+        else:
+            raise InsufficientPaymentSources
+    else:
+        raise UserCancelled
+
--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,14 @@
 from oscar.apps.checkout.views import PaymentDetailsView as CorePaymentDetailsView
+from oscar.apps.checkout.mixins import OrderPlacementMixin
 from django.views import View
 from django.http import HttpResponse
 from django.http import HttpResponse
-from .gateway import  do_pay
+from .gateway import  do_pay, check_call_back
+from oscar.apps.payment import models
+from oscar.apps.payment.exceptions import (
+    UserCancelled,
+    InsufficientPaymentSources,
+)
 import logging
 logger = logging.getLogger('oscar.checkout')
 
@@ -10,8 +16,44 @@ logger = logging.getLogger('oscar.checkout')
 class PaymentDetailsView(CorePaymentDetailsView):
     def handle_payment(self, order_number, order_total, **payment_kwargs):
         return do_pay(self.request , order_number , order_total)
+        
 
-class CheckZarrinPalCallBack(View):
+class CheckZarrinPalCallBack(View, OrderPlacementMixin):
     def get(self, request):
-        return HttpResponse(f'result {request.GET["order_number"]}')    
+        try :
+            if check_call_back(request) :
+                amount = request.GET.get('amount')
+                source_type, is_created = models.SourceType.objects.get_or_create(
+                name='ZarrinPal')
+
+                source = source_type.sources.model(
+                    source_type=source_type,
+                    amount_allocated=amount,
+                    currency='IRR',
+                )
+
+                self.add_payment_source(source)
+                self.add_payment_event('Authorised', amount)
+              
+        except InsufficientPaymentSources as e :
+            # Exception for when a user attempts to checkout without specifying enough
+            # payment sources to cover the entire order total.
+            # Eg. When selecting an allocation off a giftcard but not specifying a
+            # bankcard to take the remainder from. 
+            pass
+        
+        except UserCancelled as e :
+            # During many payment flows,
+            # the user is able to cancel the process. This should often be treated
+            # differently from a payment error, e.g. it might not be appropriate to offer
+            # to retry the payment.
+            pass
+        
+        except Exception as e :
+            # Unhandled exception - hopefully, you will only ever see this in
+            # development.
+            logger.error("Order #%s: unhandled exception while taking payment (%s)", request.GET.get("order_number"), e)
+            logger.exception(e)
+        
+        return HttpResponse(f'result {request.GET.get("order_number")}')    
 
