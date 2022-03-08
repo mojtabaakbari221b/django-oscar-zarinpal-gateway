5fa028079c1efc001ec7edb10706cae982c18b68
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-03-08 14:29:56 +0330

    add settings file for payment configuration

--- settings.py
+++ settings.py
@@ -1,17 +1,21 @@
 from django.conf import settings
+from django.contrib.sites.models import Site
+
+site = Site.objects.get_current()
+DOMAIN = site.domain
 
 DEFAULT_ZARINPAL_CONFIGURATION = {
     'MECHANT_ID' : 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX',
     'WEBSERVICE' : 'https://sandbox.zarinpal.com/pg/services/WebGate/wsdl',
-    'DESCRIPTION' : 'ZARINPAL DJANGO OSCAR GATEWAY',
     'USE_SANDBOX' : True,
+    'STARTPAY_URL': 'https://sandbox.zarinpal.com/pg/StartPay/',
 }
 
 ZARINPAL_CONFIGURATION = getattr(settings , "ZARINPAL_CONFIGURATION" , DEFAULT_ZARINPAL_CONFIGURATION)
 
 MMERCHANT_ID = ZARINPAL_CONFIGURATION.get("MECHANT_ID")
-ZARINPAL_WEBSERVICE = ZARINPAL_CONFIGURATION.get("WEBSERVICE")
-DESCRIPTION = ZARINPAL_CONFIGURATION.get("DESCRIPTION")
+WEBSERVICE = ZARINPAL_CONFIGURATION.get("WEBSERVICE")
+STARTPAY_URL = ZARINPAL_CONFIGURATION.get("STARTPAY_URL")
 
 
 
