866a4fb3550e5afddc14fc5be63940b0a82cf318
mojtabaakbari221b <mojtaba.akbari.221b@gmail.com>
2022-03-09 21:08:25 +0330

    complete first bridge

--- checkout/bridge.py
+++ checkout/bridge.py
@@ -1,20 +1,16 @@
 from .models import ZarrinPayTransaction
 
 class Bridge():
-    def start_transaction(self, order_id, basket, total_incl_tax):
+    def start_transaction(self, order_id, basket, total_excl_tax):
         transaction = ZarrinPayTransaction.objects.create(
             order_id = order_id ,
             basket = basket , 
-            total_incl_tax = total_incl_tax ,
+            total_excl_tax = total_excl_tax ,
         )
         return transaction.id
 
     def get_transaction_from_id_returned_by_zarrinpal_request_query(self, id):
-        transaction = ZarrinPayTransaction.objects.get(id=id)
-        order_id = transaction.order_id
-        basket = transaction.basket
-        total_incl_tax = transaction.total_incl_tax
-        return (order_id, basket, total_incl_tax, total_incl_tax)
+        return ZarrinPayTransaction.objects.get(id=id)
     
     def change_transaction_type_after_pay(self, id, status) :
         transaction = ZarrinPayTransaction.objects.get(id=id)
